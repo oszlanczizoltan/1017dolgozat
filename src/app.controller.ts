7325187f24ecd4d2b8d938406c9b80a8ef23eba5
Petrik Diak <diak@petrik.hu>
2024-10-17 14:26:49 +0200

    done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { newReservationDTO } from './newReservation.dto';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -11,5 +13,55 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+  
+  
+  @Get('reservation')
+  @Render('reservation')
+  getReservation(){
+    return{
+      errors: [],
+      data: {}
+    }
+  }
+
+  @Post('newReservation')
+  newReservation(
+    @Body() reservationData: newReservationDTO,
+    @Res() response: Response
+  ){
+    const errors: string[] = [];
+    const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if(!reservationData.date || !reservationData.email || !reservationData.nev || !reservationData.viewers){
+      errors.push('Minden mezot kotelezo megadni');
+    }
+    if (!expression.test(reservationData.email)){
+      errors.push('Nem megfelelő email');
+    }
+    const reservation = {
+      nev: reservationData.nev,
+      email: reservationData.email,
+      date: reservationData.date,
+      viewers: reservationData.viewers,
+    }
+    const currentDate = new Date();
+    if (!reservation.date || new Date(reservationData.date) < currentDate){
+      errors.push('Érvénytelen dátum')
+    }
+    
+    if (errors.length > 0){
+      response.render('reservation', {
+        errors,
+        data: reservationData
+      })
+      return;
+    }
+    
+    
+    response.redirect(303, '/success');
+
+    return {
+      reservation
+    }
   }
 }
---
+++ src/newReservation.dto.ts
@@ -0,0 +1,6 @@
+export class newReservationDTO{
+  nev: string;
+  email: string;
+  date: Date;
+  viewers: number;
+}
